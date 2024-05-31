3b7acb3aae81ada61cc14ef6e49433ca6a6befe1
Tim Prince <tim.prince@gmail.com>
2024-05-31 12:48:25 -0700

    Initial commit.

---
+++ dns-watchdog.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import dns.resolver
+import requests
+import time
+import traceback
+from datetime import datetime
+
+resolver = dns.resolver.Resolver()
+
+def ping(server, resolver):
+  resolver.nameservers = [server]
+
+  try:
+    resolver.resolve('google.com')
+  except dns.resolver.LifetimeTimeout:
+    return False
+
+  return True
+
+def restart(server):
+  try:
+    requests.put(f'http://%s:5380/restart' % server)
+  except Exception:
+    print('restart failed')
+    print(traceback.format_exc())
+
+servers = ['192.168.1.2', '192.168.1.9']
+
+while True:
+  for server in servers:
+
+    print(
+      f'%s: Pinging server %s... ' % (
+        datetime.now().isoformat(timespec='seconds'),
+        server
+      ),
+      end=''
+    )
+    if not ping(server, resolver):
+      print('failed, rebooting.')
+      restart(server)
+    else:
+      print('succeeded.')
+
+  time.sleep(15)
